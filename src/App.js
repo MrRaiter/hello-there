5281b125144784a0d642d61cb48dacf41baa3fca
Alexander Shakhbalaev <ashahbalaev@gmail.com>
2021-02-02 15:22:22 +0300

    Update

--- src/App.js
+++ src/App.js
@@ -2,31 +2,54 @@ import { useEffect, useRef, useState } from 'react';
 import './App.css';
 import video from './videos/MiddleEastMan01.webm';
 
+const epsEq = (val1, val2) => Math.abs(val1 - val2) < 0.1
+
 function App() {
   const videoRef = useRef(null);
   const [value, setValue] = useState(0.1);
   const [oldValue, setOldValue] = useState(0.1);
 
-  const handleChange = (e) => setValue(e.target.value)
+  const interval = useRef(null);
+  const actualValue = useRef(value);
+  const handleChange = (e) => {
+    setValue(e.target.value);
+    const delta = (e.target.value - actualValue.current) / 100;
+    console.log(delta);
 
-  useEffect(() => {
-    const duration = videoRef.current.duration;
-    console.log(value);
-    if (value >= oldValue) {
-      for (let i = oldValue; i < value; i++) {
-        const newTime = duration * parseInt(i) / 100
-        console.log('1', newTime)
-        videoRef.current.currentTime = newTime;
-      }
-    } else {
-      for (let i = oldValue; i > value; i--) {
-        const newTime = duration * parseInt(i) / 100
-        console.log('2', newTime)
-        videoRef.current.currentTime = newTime;
-      }
+    if (interval.current) {
+      clearInterval(interval.current);
     }
-    setOldValue(value);
-  }, [value])
+
+    let i = 0;
+
+    interval.current = setInterval(() => {
+      actualValue.current += delta;
+      videoRef.current.currentTime = videoRef.current.duration * actualValue.current / 100;
+      if (i++ === 100) {
+        clearInterval(interval.current);
+        interval.current = null;
+      }
+    }, 10);
+  }
+
+  // useEffect(() => {
+  //   const duration = videoRef.current.duration;
+  //   console.log(value);
+  //   if (value >= oldValue) {
+  //     for (let i = oldValue; i < value; i++) {
+  //       const newTime = duration * parseInt(i) / 100
+  //       console.log('1', newTime)
+  //       videoRef.current.currentTime = newTime;
+  //     }
+  //   } else {
+  //     for (let i = oldValue; i > value; i--) {
+  //       const newTime = duration * parseInt(i) / 100
+  //       console.log('2', newTime)
+  //       videoRef.current.currentTime = newTime;
+  //     }
+  //   }
+  //   setOldValue(value);
+  // }, [value])
 
   return (
     <div className="App">
